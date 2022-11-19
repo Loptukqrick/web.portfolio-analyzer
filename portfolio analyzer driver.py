12a4e266bb795d71e3e5e7da3b5f109491db668a
Loptukqrick <48658100+Loptukqrick@users.noreply.github.com>
2022-11-19 14:33:46 -0700

    Update portfolio analyzer driver.py

--- portfolio analyzer driver.py
+++ portfolio analyzer driver.py
@@ -5,133 +5,125 @@ import numpy
 # Place holder to simulate user input
 user_input_arr = ['AAPL', 'CSX', 'PBR', 'VTWO', 'DBA']
 
-# This loop selects the column we care about from each
-# stock's data frame (closing price)
-time_frame_arr = []
-count = 0
-for i in user_input_arr:
-    stock = yf.Ticker(i)
-    data = stock.history(period="max")
-    close = data['Close']
-    time_frame_arr.append(close)
-    count += 1
+# takes list of stock picks and creates 
+# dataframe of closing prices for all of 
+# the stocks for the longest possible time frame 
+# (limited by smallest lifespan) (input is user stock list)
+def organize_data_func(arr):
+    stock_list = arr
+    size = len(stock_list)-1
+    time_frame_arr = []
+    length_arr = []
+    trimmed_data = []
+    count = 0
+    for i in stock_list:
+        stock = yf.Ticker(i)
+        data = stock.history(period="max")
+        close = data['Close']
+        time_frame_arr.append(close)
+        count += 1
+        
+    for i in time_frame_arr:
+        length_arr.append(len(i))
+    length_arr.sort()
+    smallest_period = length_arr[0]
     
-# Determines the shortest dataframe column
-# or the stock with the shorest history. Any portfolio
-# analysis is limited by the stock with the shorest lifespan
-length_arr = []
-for i in time_frame_arr:
-    length_arr.append(len(i))
-length_arr.sort()
-smallest_period = length_arr[0]
+    trimmed_data = []
+    for i in time_frame_arr:
+        trim = i.tail(smallest_period) 
+        trimmed_data.append(trim)
+    portfolio_dataframe = pd.concat(trimmed_data, axis=1, keys=user_input_arr)
+    return portfolio_dataframe
 
-# Assembles columns of equivalent length for each stock,
-# with the length being determined above by the stock with 
-# the shortest lifespan
-trimmed_data = []
-for i in time_frame_arr:
-    trim = i.tail(smallest_period) 
-    trimmed_data.append(trim)
-portfolio_dataframe = pd.concat(trimmed_data, axis=1, keys=user_input_arr)
-portfolio_dataframe
+return_table = organize_data_func(user_input_arr)
 
-# Prints a correlation heatmap which is useful information for 
-# rebalance strategies
 import seaborn as sb
-corr = portfolio_dataframe.corr()
+corr = organize_data_func(user_input_arr).corr()
 sb.heatmap(corr, cmap="Blues", annot=True)
-#############
+
+period = 5
+# This function takes every nth element from each stock price list
+# to use as reference for rebalance. In between rebalance periods, we don't
+# really care what the stock price is.
 
 
-##################
-# user input for rebalance, period.
-rebalance_period = 5
-
-# for some reason, rebalance_period adds 2 to the value when using iloc.
-# so rebalance_period=7 is actually 9. That's why it is equal to '5' here.
-# really strange counting issue.
-# This determines the frequency of rebalance by pulling every nth element
-# and storing it in 'rebalance_dates.' Iterating every 7th row gives weekly 
-# rebalance, and iterating every 30th row gives monthly rebalance, etc.
-rebalance_dates = portfolio_dataframe.iloc[::rebalance_period, :]
-print(rebalance_dates)
-
-
-# user_input_arr <-- list of stock tickers
-# Each column in rebalance_dates is converted to a numpy array
-# for more efficient calculations.
-price_list = []
-for i in user_input_arr:
-    price_list.append(rebalance_dates[i].to_numpy())
-
-
-# The data we have been working with hither-to-fore has been
-# closing prices.  To run calculations, we calculate the growth 
-# rate between each row by calculating (i+1)/i
-return_table = []
-return_list = []
-for i in price_list:
-    for j in range(len(i-1)):
-        try:
-            res = i[j+1]/i[j]
-            return_list.append(res)
-        except: 
-            pass
-    return_table.append(return_list)
+# converts dataframe columns to numpy arrays for faster calculations 
+# and converts prices into returns on rebalancing time preferences.  
+# (input is rebalance period)
+def make_return_percentages(rebalance_period, stocks_arr, stock_table):
+    table = stock_table
+    rebalance_dates = table.iloc[::rebalance_period, :]
+    # user_input_arr <-- list of stock tickers
+    # Each column in rebalance_dates is converted to a numpy array
+    # for more efficient calculations.
+    price_list = []
+    for i in stocks_arr:
+        price_list.append(rebalance_dates[i].to_numpy())
+    performance_table = []
     return_list = []
-#print(len(return_table[0]))
+    for i in price_list:
+        for j in range(len(i-1)):
+            try:
+                res = i[j+1]/i[j]
+                return_list.append(res)
+            except: 
+                pass
+        performance_table.append(return_list)
+        return_list = []
+    return performance_table
+
+percent_table = make_return_percentages(period, user_input_arr, return_table)
+
+# calculates 'buy and hold' returns
+def buy_and_hold(weightings, roi_table):
+    buy_and_hold_portfolio = weightings
+    calc_table = roi_table
+    index = 0
+    for i in calc_table:
+        for j in i:
+            buy_and_hold_portfolio[index] = buy_and_hold_portfolio[index]*j
+        index+=1
+    #print('buy and hold: ', buy_and_hold_portfolio)
+    end_val = sum(buy_and_hold_portfolio)
+    #print('end value: $', format(end_val, ',.2f'))
+    #print(' ')
+    return end_val, buy_and_hold_portfolio
+
+# calculates tactical rebalance returns.
+def tactical_rebalance(weightings, roi_table):
+    index = 0
+    rebalance_portfolio = weightings
+    calc_table = roi_table
+    for i in calc_table:
+        for j in i:
+            rebalance_portfolio[index] = rebalance_portfolio[index]*j
+            value = sum(rebalance_portfolio)
+            rebalance_portfolio[0] = value/len(rebalance_portfolio)
+            rebalance_portfolio[1] = value/len(rebalance_portfolio)
+            rebalance_portfolio[2] = value/len(rebalance_portfolio)
+            rebalance_portfolio[3] = value/len(rebalance_portfolio)
+            rebalance_portfolio[4] = value/len(rebalance_portfolio)
+        index+=1
+    #print('rebalancing: ', rebalance_portfolio)
+    end_val = sum(rebalance_portfolio)
+    #print('end value: $', format(end_val, ',.2f'))
+    return end_val, rebalance_portfolio
 
 
-# max time frame for user selected investments
-# This tells us how long each column is in our table, which
-# gives us our investment time frame.  This will be important
-# later to calculate annual return in conjunction with final
-# portfolio value.
-investment_time_frame = len(return_table[0])
-print(investment_time_frame)
-print('beginning:', price_list[4][0])
-print('end:', price_list[4][len(price_list[4])-1])
-#####################
+# need to customize weightings
+# main:
+portfolio_weightings = [200, 200, 200, 200, 200]
+print('1: ', portfolio_weightings)
+buy_and_hold_result = buy_and_hold(portfolio_weightings, percent_table)
+print('2: ', portfolio_weightings)
 
-####################
-# The portfolio weightings/dollar amounts will be selected by the user,
-# but the dummy data is a portfolio of $1000 with 20% weightings in 
-# 5 stocks.
-##                        DBA, IPI, VTWO, PBR, CSX
-buy_and_hold_portfolio = [200, 200, 200, 200, 200]
+# Need to reset portfolio weightings here before value is called again:
+# portfolio_weightings should be global, but somehow it is changed once
+# passed into the function... ask Daniel
+portfolio_weightings = [200, 200, 200, 200, 200]
+print('3: ', portfolio_weightings)
+tactical_rebal_result = tactical_rebalance(portfolio_weightings, percent_table)
+print('4: ', portfolio_weightings)
 
-
-# Buy and hold loop (should be a function)
-index = 0
-for i in return_table:
-    for j in i:
-        buy_and_hold_portfolio[index] = buy_and_hold_portfolio[index]*j
-    index+=1
-print('buy and hold: ', buy_and_hold_portfolio)
-end_val = sum(buy_and_hold_portfolio)
-print('end value: $', format(end_val, ',.2f'))
-print(' ')
-
-
-# Rebalance portfolio with equal weightings for $1000
-rebalance_portfolio = [200, 200, 200, 200, 200]
-
-# Rebalance loop
-index = 0
-for i in return_table:
-    for j in i:
-        rebalance_portfolio[index] = rebalance_portfolio[index]*j
-        ## rebalancing code:
-        #-----
-        value = sum(rebalance_portfolio)
-        rebalance_portfolio[0] = value/len(rebalance_portfolio)
-        rebalance_portfolio[1] = value/len(rebalance_portfolio)
-        rebalance_portfolio[2] = value/len(rebalance_portfolio)
-        rebalance_portfolio[3] = value/len(rebalance_portfolio)
-        rebalance_portfolio[4] = value/len(rebalance_portfolio)
-        #-----
-    index+=1
-print('rebalancing: ', rebalance_portfolio)
-end_val = sum(rebalance_portfolio)
-print('end value: $', format(end_val, ',.2f'))
-###############
+print('buy and hold: ', buy_and_hold_result)
+print('tactical_rebal_result: ', tactical_rebal_result)
